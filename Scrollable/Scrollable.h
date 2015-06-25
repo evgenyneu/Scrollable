b1c9d87369f2e711a9c08c3ec407dd0a149d09a5
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-25 13:43:15 +1000

    Add framework

---
+++ Scrollable/Scrollable.h
@@ -0,0 +1,19 @@
+//
+//  Scrollable.h
+//  Scrollable
+//
+//  Created by Evgenii on 25/06/2015.
+//  Copyright © 2015 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Scrollable.
+FOUNDATION_EXPORT double ScrollableVersionNumber;
+
+//! Project version string for Scrollable.
+FOUNDATION_EXPORT const unsigned char ScrollableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scrollable/PublicHeader.h>
+
+
---
+++ ScrollableTests/ScrollableTests.swift
@@ -0,0 +1,35 @@
+//
+//  ScrollableTests.swift
+//  ScrollableTests
+//
+//  Created by Evgenii on 25/06/2015.
+//  Copyright © 2015 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+import XCTest
+
+class ScrollableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
